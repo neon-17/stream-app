5e5c618de423a9e76de2448c7f564df7a3eeee35
dharmesh <dharmesh.convegenius@gmail.com>
2020-01-05 18:50:54 +0530

    db connection refactoring in subscriber

--- src/subscribers/NotificationSubscriber.js
+++ src/subscribers/NotificationSubscriber.js
@@ -2,8 +2,8 @@ const path = require('path');
 const Notification = require(path.join(__dirname, '..', 'services', 'NotificationService')).Notification
 const config = require(path.join(__dirname, '..', 'config'));
 const eventConsumer = new Notification(config.notification_event_queue);
-const mongoose = require('mongoose');
-mongoose.connect(config.db_url, { useNewUrlParser: true });
+
+require(path.join(__dirname, '..', 'loaders', 'connection'));
 
 (async function() {
     await eventConsumer.processEvent(eventConsumer.insertEvent);
